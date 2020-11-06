ef17c8b588a9ee810b10bcfc33ce69d49b6980e2
Vishnu <vishnu.m@nativewit.in>
2020-11-06 19:36:50 +0530

    multiply

--- multiplywithoutloops.dart
+++ multiplywithoutloops.dart
@@ -1,10 +1,10 @@
 void main() {
   List<int> num = [1, 2, 5, 4];
   int value = 3;
-  findValue(num, value);
+  multiply(num, value);
 }
 
-void findValue(List<int> n, int x) {
+void multiply(List<int> n, int x) {
   List<int> newList = <int>[];
   n.forEach((element) {
     element = element * x;
