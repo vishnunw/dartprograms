4d80edc9d249d2e8c76adae393fcf59344926906
Vishnu <vishnu.m@nativewit.in>
2020-11-06 14:00:20 +0530

    multiplication

--- multiplicationtable.dart
+++ multiplicationtable.dart
@@ -7,14 +7,12 @@ void main() {
 }
 
 void multiplicationTable(int b) {
-  
   int i = 1;
-  while (i <= b){
-    for (int j=1; j<=10; j++){
-      print("$i * $j = ${i*j}");
+  while (i <= b) {
+    for (int j = 1; j <= 10; j++) {
+      print("$i * $j = ${i * j}");
     }
     print("\n");
     i++;
-    
   }
 }
