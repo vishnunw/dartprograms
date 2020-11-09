68127a1a9914f02e8d2a45988fdb762a984e640b
vishnu <vishnu.m@nativewit.in>
2020-11-09 18:22:04 +0530

    check and update map

--- updatekeyvalue.dart
+++ updatekeyvalue.dart
@@ -1,8 +1,8 @@
-//Check if a <K,V> pair is present in a map and add it without using []
+//Update a <K,V> pair in map without using [] and insert the element if it is not found
 
 void main() {
   Map<int, String> mapOne = {1: 'Vishnu', 2: 'bala'};
-  Map<int, String> mapTwo = {1: 'vishnu', 4: 'bals'};
+  Map<int, String> mapTwo = {3: 'vishu', 4: 'bals'};
 
   mapExamples(mapOne, mapTwo);
 }
@@ -12,8 +12,9 @@ void mapExamples(Map<int, String> mapsOne, Map<int, String> mapsTwo) {
       mapsOne.containsValue(mapsTwo.values)) {
     mapsOne.remove(mapsTwo.values);
     print(mapsOne);
+    print(mapsTwo);
   } else {
-    mapsOne.addAll(mapsTwo);
+    mapsOne.addEntries(mapsTwo.entries);
     print(mapsOne);
   }
 }
