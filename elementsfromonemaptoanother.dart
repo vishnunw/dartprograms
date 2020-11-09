2d636da0fe3d4cc6e436631531066302edc988e3
vishnu <vishnu.m@nativewit.in>
2020-11-09 18:02:05 +0530

    check if<K,V> is in a map and add it without []

--- elementsfromonemaptoanother.dart
+++ elementsfromonemaptoanother.dart
@@ -1,9 +1,19 @@
-// Add elements from one map to another
+//Check if a <K,V> pair is present in a map and add it without using []
 
 void main() {
   Map<int, String> mapOne = {1: 'Vishnu', 2: 'bala'};
-  Map<int, String> mapTwo = {3: 'vish', 4: 'bals'};
+  Map<int, String> mapTwo = {1: 'vishnu', 4: 'bals'};
 
-  mapOne.addAll(mapTwo);
-  print(mapOne);
+  mapExamples(mapOne, mapTwo);
+}
+
+void mapExamples(Map<int, String> mapsOne, Map<int, String> mapsTwo) {
+  if (mapsOne.containsKey(mapsTwo.keys) &&
+      mapsOne.containsValue(mapsTwo.values)) {
+    mapsOne.removeWhere((key, value) => true);
+    print(mapsOne);
+  } else {
+    mapsOne.addAll(mapsTwo);
+    print(mapsOne);
+  }
 }
